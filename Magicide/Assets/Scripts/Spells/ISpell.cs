87ed5eaac51f63295e7778fdad4f4cb37939bb9c
Will Garrison <will.v.garrison@gmail.com>
2025-05-12 21:34:26 -0500

    Added base spell interface and spell types

---
+++ Magicide/Assets/Scripts/Spells/ISpell.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace Spell
+{
+    // The types of spells
+    public enum SpellType
+    {
+        None = 0,
+        Fire,
+        Water,
+        Earth,
+        Air,
+        Lightning,
+        Space,
+        Time,
+        Shadow,
+        Light,
+        Heal
+    }
+
+    // Interfaces: https://www.w3schools.com/cs/cs_interface.php
+    // A reference class for all spells
+    public interface ISpell
+    {
+        // Damage of the spell
+        float Damage { get; }
+
+        // Amount of mana consumed when cast
+        float ManaCost { get; }
+
+        // Amount of time taken when casting
+        float CastTime { get; }
+
+        // Is spell an area of affect spell
+        bool IsAreaOfAffect { get; }
+
+        // The spell type of spell
+        SpellType Type { get; }
+
+        // NOTE: Maybe add casting wand to properties
+
+        // Casts the spell
+        void Cast();
+    }
+}
