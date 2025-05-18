6487553a069f4671c20fddc07d8967c99d22ee72
GabeTimesCode <gmwthiems@gmail.com>
2025-05-18 12:41:11 -0500

    Made the triangle spell test move
    Updated a script so now you can summon a moving triangle with K.

--- Magicide/Assets/Scripts/Misc. Test/MakeTriangleAppearTEST.cs
+++ Magicide/Assets/Scripts/Misc. Test/MakeTriangleAppearTEST.cs
@@ -4,22 +4,33 @@ public class MakeTriangleAppearTEST : MonoBehaviour
 {
     public GameObject AppearingObject;
     public Rigidbody2D playerBody;
+    public float AppearingObjectLifeSpan = 2f;
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
+    { }
         
-    }
+
+
 
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.K))
         {
             Vector2 AppearingObjectPosition = playerBody.position;
 
-            Instantiate(AppearingObject, AppearingObjectPosition, Quaternion.identity);
+            GameObject AppearedObject = Instantiate(AppearingObject, AppearingObjectPosition, Quaternion.identity);
+
+
+            AppearedObject.GetComponent<Rigidbody2D>().linearVelocity = playerBody.linearVelocity;
+            Destroy(AppearedObject, AppearingObjectLifeSpan);
+
         }
-            
+
     }
 }
+
